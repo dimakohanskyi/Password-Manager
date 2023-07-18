4581e00ea5d037913412ce853db20eae233fa4e8
dimakohanskyi <dimakohanskyi@gmail.com>
2023-07-18 21:26:32 +0300

    Make plan and done UI part

---
+++ main.py
@@ -0,0 +1,70 @@
+from tkinter import *
+
+
+
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
+
+
+
+
+
+
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+
+
+
+
+
+
+
+
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=0)
+
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+website_input = Entry(width=35)
+website_input.grid(column=1, row=1, columnspan=2)
+website_input.focus()
+
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+email_input = Entry(width=35)
+email_input.grid(column=1, row=2, columnspan=2)
+email_input.insert(0, "dimakohanskyi@gmail.com")
+
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+password_input = Entry(width=17)
+password_input.grid(column=1, row=3)
+
+
+generate_password = Button(text="Generate Password")
+generate_password.grid(column=2, row=3)
+
+
+add_button = Button(text="Add", width=30)
+add_button.grid(column=1, row=4, columnspan=2)
+
+
+window.mainloop()
